3ddc52cf18cded3a45c22d7965ded0be803aa048
Manav Patel <manavpatel489@gmail.com>
2026-02-04 12:01:22 +0530

    Update water.js

--- water/water.js
+++ water/water.js
@@ -2,6 +2,7 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+// 
 const updateBigCup = () => {
   const fullCups = document.querySelectorAll(".cup-small.full").length;
   const totalCups = smallCups.length;
